d48e029d094db957f6068df65e82c96b0f9c459d
Hugo Tavares <hm533@cam.ac.uk>
2026-08-20 11:24:19 +0100

    add script to download course data for local development

---
+++ course_files/download_dropbox.sh
@@ -0,0 +1,7 @@
+#!/usr/env bash
+
+wget -O course_files.zip "https://www.dropbox.com/scl/fo/tnlv39c7tummmgfgus9ke/AKcdC6Cw26JFsbkWZV9nsRE?rlkey=go3spp4tabdzkbp60gh481l10&st=zfrl0oi2&dl=1"
+
+unzip course_files.zip
+
+rm course_files.zip
